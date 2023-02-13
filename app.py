9b8ff3228a2f05c13491095142562fe3c0f2da0c
Jean <jeanoi4212@gmail.comm>
2023-02-13 08:43:49 -0500

    mejorando

--- app.py
+++ app.py
@@ -1,32 +1,16 @@
-from flask import Flask, render_template, url_for
+from flask import Flask
+from routes.views import index, trabajadores, planilla, asistencia, login
 
 app = Flask(__name__)
 
-data = [{"paterno": "Oropeza", "materno": "Inca", "nombre": "Jeancarlos Alberto",
-         "dni": "48555618", "cargo": "Asistente", "distrito": "Chorrillos",
-         "licencia": "A48555618", "categoria": "AIIIC", "revalidacion":"01/01/2022",
-         "nacimiento": "01/01/2000", "ingreso": "01/01/2022"}, 
-         {"paterno": "Garcia", "materno": "Guivin", "nombre": "Claret Magna",
-           "dni": "40137921", "cargo": "Empleada", "distrito": "Surco",
-           "licencia": "A40137921", "categoria": "AIIB", "revalidacion":"10/11/2022",
-            "nacimiento": "05/04/1990", "ingreso": "31/12/2019"}]
-
-@app.route("/")
-def index():    
-    return render_template("index.html")
-
-@app.route("/trabajadores")
-def trabajadores():    
-    return render_template("trabajadores.html", data=data)
-
-@app.route("/planilla")
-def planilla():    
-    return render_template("planilla.html")
-
-@app.route("/asistencia")
-def asistencia():    
-    return render_template("asistencia.html")
-
+app.add_url_rule("/", view_func=index)
+app.add_url_rule("/trabajadores", view_func=trabajadores)
+app.add_url_rule("/planilla", view_func=planilla)
+app.add_url_rule("/asistencia", view_func=asistencia)
+app.add_url_rule("/login", view_func=login)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
