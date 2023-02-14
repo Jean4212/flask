02c6de69d0f7e4f672abd0bf2d232eb63e739610
Jean <jeanoi4212@gmail.com>
2023-02-14 18:25:29 -0500

    mejorando

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask
-from routes.views import index, trabajadores, planilla, asistencia, login
+from routes.views import login, index, trabajadores, asistencia, desarrollo, planilla
 
 app = Flask(__name__)
 
+app.add_url_rule("/login", view_func=login)
 app.add_url_rule("/", view_func=index)
 app.add_url_rule("/trabajadores", view_func=trabajadores)
-app.add_url_rule("/planilla", view_func=planilla)
 app.add_url_rule("/asistencia", view_func=asistencia)
-app.add_url_rule("/login", view_func=login)
+app.add_url_rule("/desarrollo", view_func=desarrollo)
+app.add_url_rule("/planilla", view_func=planilla)
 
 if __name__ == "__main__":
     app.run(debug=True)
