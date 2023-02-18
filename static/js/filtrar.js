ba3c7d0cb176e2dde96b45c871255b9eebb3c3d1
Jean <jeanoi4212@gmail.com>
2023-02-17 19:27:02 -0500

    mejorando

--- static/js/filtrar.js
+++ static/js/filtrar.js
@@ -21,19 +21,3 @@ document.getElementById("search").addEventListener("search", function(event) {
         }
     }
   );
-
-  function addRowHandlers() {
-    var table = document.getElementById("tableId");
-    var rows = table.getElementsByTagName("tr");
-    for (i = 0; i < rows.length; i++) {
-      var currentRow = table.rows[i];
-      var createClickHandler = function(row) {
-        return function() {
-          var cell = row.getElementsByTagName("td")[0];
-          var id = cell.innerHTML;
-          alert("id:" + id);
-        };
-      };
-      currentRow.onclick = createClickHandler(currentRow);
-    }
-  }
