b11836c4d362f209cbcff5dc802ce6976b1d74c0
Jean <jeanoi4212@gmail.com>
2023-02-20 17:56:47 -0500

    mejorando

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from routes.views import login, index, trabajadores, asistencia, desarrollo, planilla, admin
 
 db = SQLAlchemy()
+
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database/db.sqlite3"
+app.secret_key = "Secret key"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.getcwd() + '\database\database.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+
 db.init_app(app)
+
+from models.model import User
+
+with app.app_context():
+    db.create_all()
+
 
 app.add_url_rule("/login", view_func=login)
 app.add_url_rule("/", view_func=index)
@@ -13,10 +26,9 @@ app.add_url_rule("/trabajadores", view_func=trabajadores)
 app.add_url_rule("/asistencia", view_func=asistencia)
 app.add_url_rule("/desarrollo", view_func=desarrollo)
 app.add_url_rule("/planilla", view_func=planilla)
-
 app.add_url_rule("/admin", methods=["GET", "POST"], view_func=admin)
 
-if __name__ == "__main__":
+if __name__ == "__main__":      
     app.run(debug=True)
 
 
