8fd317791c1c2da6a14ba652abd71957d6b7da6a
Jean <jeanoi4212@gmail.comm>
2023-02-20 08:26:20 -0500

    mejorando

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from routes.views import login, index, trabajadores, asistencia, desarrollo, planilla, admin
 
+db = SQLAlchemy()
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database/db.sqlite3"
+db.init_app(app)
 
 app.add_url_rule("/login", view_func=login)
 app.add_url_rule("/", view_func=index)
